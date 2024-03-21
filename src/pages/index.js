26e0f3b6e5aadb1050200a1f0dfde76b2df56466
Niara M <niaraelyce@Niaras-MacBook-Air.local>
2024-03-21 18:22:44 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Button from "../../components/Button";
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <Button label="Download" />
+      <Button label="Register Now" />
+      <Button label="Learn More" />
+    </div>
+  );
+};
+export default Homepage;
